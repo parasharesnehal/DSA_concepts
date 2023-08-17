fc536c3c3900a0d9aa5101edf71b0d8bb3afa5ec
Snehal Parashare <98743873+parasharesnehal@users.noreply.github.com>
2023-08-17 10:39:00 -0700

    Add files via upload

---
+++ examples_recursion/taylorseries.c
@@ -0,0 +1,20 @@
+//1+(x/1)+(x2/2)+(x3/3)
+
+#include<stdio.h>
+double e(int x, int n)
+{
+ static double p=1,f=1;
+ double r;
+
+ if(n==0)
+ return 1;
+ r=e(x,n-1);
+ p=p*x;
+ f=f*n;
+ return r+p/f;
+}
+int main()
+{
+ printf("%lf \n",e(4,15));
+ return 0;
+}
